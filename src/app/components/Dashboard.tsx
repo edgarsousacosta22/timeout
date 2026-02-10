4e0d0c2d0f1336df89cf4077b0ec17b75dec745b
RafaelGomesWm <rafael.gomes@webminds.pt>
2026-02-10 17:59:33 +0000

    Update Dashboard.tsx
    Toques finais 3(botões)

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -184,13 +184,14 @@ export const Dashboard = ({ user, onLogout }: DashboardProps) => {
 
   // Logic for unlocking buttons based on the description
   const isSaidaBloqueada = !!punches.saida;
+  const isAlmocoBloqueado = !!punches.almoco;
   
   const canEntrada = !punches.entrada && !isSaidaBloqueada && !isProcessing;
   const canSaida = !!punches.entrada && !punches.saida && !isProcessing;
-  const canPausa = !!punches.entrada && !punches.pausa && !isSaidaBloqueada && !isProcessing;
-  const canVoltaPausa = !!punches.pausa && !punches.voltaPausa && !isSaidaBloqueada && !isProcessing;
-  const canAlmoco = !!punches.entrada && !punches.pausa && !punches.voltaPausa && !punches.almoco && !isSaidaBloqueada && !isProcessing;
-  const canVoltaAlmoco = !!punches.almoco && !punches.pausa && !punches.voltaPausa && !punches.voltaAlmoco && !isSaidaBloqueada && !isProcessing;
+  const canPausa = !!punches.entrada && !punches.pausa && isAlmocoBloqueado && !isProcesing && !isSaidaBloqueada && !isProcessing;
+  const canVoltaPausa = !!punches.pausa && !punches.voltaPausa && isAlmocoBloqueado && !isProcesing && !isSaidaBloqueada && !isProcessing;
+  const canAlmoco = !!punches.entrada && !punches.almoco && !isSaidaBloqueada && !isProcessing;
+  const canVoltaAlmoco = !!punches.almoco && !punches.voltaAlmoco && !isSaidaBloqueada && !isProcessing;
 
   const InfoRow = ({ label1, value1, label2, value2, label3,value3 }: { label1: string, value1: string | null, label2: string, value2: string | null}) => (
     <div className="grid grid-cols-2 gap-4 py-2 border-b border-slate-100 last:border-0">
