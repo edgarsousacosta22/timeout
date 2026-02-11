b510dc62973f2c00bb5f8c1f20e9701a5fc538fc
RafaelGomesWm <rafael.gomes@webminds.pt>
2026-02-11 10:20:08 +0000

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -34,7 +34,7 @@ const App = () => {
           {!currentUser ? (
             <>
               <div className="flex items-center justify-center -mt-[60px] relative z-20">
-                <img src={logo} alt="Logotipo" className="h-80" />
+                <img src={logo} alt="Logotipo" className="h-100" />
               </div>
               <PinKiosk onSuccess={handleLoginSuccess} />
             </>
